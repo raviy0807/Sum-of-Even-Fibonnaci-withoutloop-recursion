69d890e1162a85d78dd4758251f2e587f4586c99
Ravi Yadav <ravi.y0807@gmail.com>
2019-05-07 23:39:11 +0100

    Logic: Code Implementation
    THe commit contains the code which calculate the Geometric summation.

--- main.cpp
+++ main.cpp
@@ -5,16 +5,33 @@ using namespace std;
 using namespace boost::multiprecision;
 
 class Fibonacci{
-   
+    float128 phi;
+    float128 a;
+    // Source : http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibFormula.html
+    // Based on the source, we reduce the formula to on phi as 'V' contribute neglagible and its distance from zero keeps on increasing with sequence.
+
     float128 even_sum(int num){
+
+        // Calculate the Gemotric Sum i.e. Sum = a * ((1+r**n)/(1-r))
+        // n = (1 + r **n )
+        float128 n = 1 - pow(phi,3*num);
+        // d = (1 - r)
+        float128 d = 1 - pow(phi,3);
+        // Gm of even number = a * (n/d)
+        float128 sum = a * (n/d);
+
+        return sum;
     }
 
 public:
 
     Fibonacci(){
+        phi = (1+sqrt(5))/2;
+        a = pow(phi,3)/sqrt(5);
     }
 
     unsigned long long int get_even_sum(int limit){
+        
     }
 };
 
